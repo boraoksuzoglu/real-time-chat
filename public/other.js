4677fe3bc293762e52c697cea6569731bb0a255f
Bora Öksüzoğlu <boraoksuzoglu@gmail.com>
2022-10-08 15:25:21 +0300

    Add ESLint

--- public/other.js
+++ public/other.js
@@ -1,1 +1,3 @@
-document.getElementById("users").style.maxHeight = `${document.getElementById("users").childNodes[1].clientHeight * 4}px`
+document.getElementById('users').style.maxHeight = `${
+	document.getElementById('users').childNodes[1].clientHeight * 4
+}px`
